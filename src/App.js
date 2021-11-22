59ad9c39c9029057c70de3b3c928420c0cd4216b
Iskren Ivanov <pecito_vn@hotmail.com>
2021-11-22 16:06:39 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
+import { Routes, Route } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+
 import './App.css';
+import Header from './components/Header/Header';
+import Dashboard from './components/Dashboard/Dashboard';
+import Login from './components/Login/Login';
+import Register from './components/Register/Register';
+import MyPets from './components/MyPets/MyPets';
+import Create from './components/Create/Create';
+import Details from './components/Details/Details';
+import * as authService from './services/authService';
+
+
 
 function App() {
+  const [userInfo, setUserInfo] = useState({ isAuthenticated: false, username: '' });
+
+  useEffect(() => {
+    let user = authService.getUser();
+
+    setUserInfo({
+      isAuthenticated: Boolean(user),
+      user,
+    });
+
+  }, []);
+
+  function onLogin(username) {
+    setUserInfo({
+      isAuthenticated: true,
+      user: username,
+    });
+  };
+
+  // const onLogout = () => {
+  //   setUserInfo({
+  //     isAuthenticated: false,
+  //     user: null,
+  //   });
+  // };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="container">
+      <Header {...userInfo} />
+
+      <main id="site-content">
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/login" element={<Login onLogin={onLogin} />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/my-pets" element={<MyPets />} />
+          <Route path="/create" element={<Create />} />
+          <Route path="/details/:petId" element={<Details />} />
+        </Routes>
+      </main>
+
+      <footer id="site-footer">
+        <p>@PetMyPet</p>
+      </footer>
     </div>
   );
 }
